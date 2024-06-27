ae757ecf71e9b51a7e47d4e17b5cfef3e2465613
Raihan Hasan <raihanhasan@Raihans-Laptop.attlocal.net>
2024-06-27 19:43:30 -0400

    05 spec done

--- aa05-hash-tablesecho/phases/01-implementation.js
+++ aa05-hash-tablesecho/phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here 
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null);
   }
 
   hash(key) {
@@ -30,12 +31,68 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here 
+    // let index = this.hashMod(key);//position index in the data array
+    // let kvp = new KeyValuePair(key,value);
+    // if(!this.data[index])
+    //   //checking the index in the data array to see if there is already a key-value
+    //   {
+    //       this.data[index] = kvp;
+
+    // }
+    // else{
+    //   kvp.next = this.data[index];
+    //   this.data[index] = kvp;
+    //   this.count++
+    // }
+
+    let index = this.hashMod(key);//position index in the data array
+    let kvp = new KeyValuePair(key,value);
+    let currPair = this.data[index];//if there anything in the index we are assining to the varriable
+    while(currPair && currPair.key !== key){
+        currPair = currPair.next
+    }
+
+    if(currPair){//when the key matches to our new key
+      currPair.value = value;//update the value
+    }
+    else{
+      //if the bucket in empty
+      if(!this.data[index]){
+        this.data[index] = kvp;
+      }
+      else{
+        //if the bucket in not empty
+        //assign the ole 0ne into the our kvps next
+        // set the kvp to the new head of the linked list
+        kvp.next = this.data[index];
+        this.data[index] = kvp;
+
+      }
+    }
+    this.count++
+  
   }
 
 
   read(key) {
-    // Your code here 
+    let index = this.hashMod(key);
+    if(this.data[index].key !== key){
+      
+      return undefined
+    }
+    
+      let currPair = this.data[index];
+      while(currPair.key===key){
+        currPair = currPair.next;//walk through the key-value pair in this index
+        if(currPair.key === key){
+         return  currPair.value
+        }
+      }
+    
+    
+    
+
+    return this.data[index].value;
   }
 
 
